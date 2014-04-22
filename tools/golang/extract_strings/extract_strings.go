500ceab39c21e7699a1a8dbddafddfb3783f0184
dr.max <maximilien@acm.org>
2014-04-22 11:33:20 -0700

    added filtering of import statements and started coverting to a class ExportString

--- tools/golang/extract_strings/extract_strings.go
+++ tools/golang/extract_strings/extract_strings.go
@@ -7,6 +7,19 @@ import (
 	"go/token"
 )
 
+type StringInfo struct {
+	Value string
+	Filename string
+	Pos int
+}
+
+type ExtactString struct {
+	ExtractedStrings []StringInfo
+	FilteredStrings map[string]string	
+}
+
+var filteredStrings map[string]string
+
 func InspectFile(fileName string) error {
 	fmt.Println("Extracting strings from file:", fileName)
 
@@ -17,6 +30,9 @@ func InspectFile(fileName string) error {
 		fmt.Println(err)
 		return err
 	}
+
+	filteredStrings = make(map[string]string)
+	addImports(astFile)
 
 	extractString(astFile, fset)
 
@@ -51,10 +67,24 @@ func extractString(f *ast.File, fset *token.FileSet) {
 		switch x := n.(type) {
 		case *ast.BasicLit:
 			s = x.Value
-			if s != "" && x.Kind == token.STRING {
+			if s != "" && x.Kind == token.STRING && !filter(s) {
 				fmt.Printf("%s:\t%s\n", fset.Position(n.Pos()), s)
 			}
 		}
 		return true
 	})
+}
+
+func addImports(astFile *ast.File) {
+		for i := range astFile.Imports {
+		filteredStrings[astFile.Imports[i].Path.Value] = astFile.Imports[i].Path.Value
+	}
+
+}
+
+func filter(aString string) bool {
+	if filteredStrings[aString] != "" {
+		return true
+	}
+	return false
 }
