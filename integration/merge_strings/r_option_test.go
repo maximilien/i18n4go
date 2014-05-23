7c3ca3dceff91b6b448462a41efd455af55b69e0
Anand Gaitonde and Simon Leung <agaitonde@pivotallabs.com>
2014-05-23 15:28:00 -0700

    test for -merge-strings '-r' option

---
+++ integration/merge_strings/r_option_test.go
@@ -0,0 +1,42 @@
+package merge_strings_test
+
+import (
+	"path/filepath"
+
+	. "github.com/maximilien/i18n4cf/integration/test_helpers"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("merge-strings -d dirName", func() {
+	var (
+		INPUT_FILES_PATH    = filepath.Join("r_option", "input_files")
+		EXPECTED_FILES_PATH = filepath.Join("r_option", "expected_output")
+	)
+
+	Context("can combine multiple language files per directory", func() {
+		BeforeEach(func() {
+			session := Runi18n("-merge-strings", "-v", "-r", "-d", filepath.Join(INPUT_FILES_PATH), "-source-language", "en")
+			Ω(session.ExitCode()).Should(Equal(0))
+		})
+
+		AfterEach(func() {
+			RemoveAllFiles(
+				GetFilePath(INPUT_FILES_PATH, "en.all.json"),
+				GetFilePath(INPUT_FILES_PATH+"/sub", "en.all.json"),
+			)
+		})
+
+		It("en.all.json contains translations from both files", func() {
+			CompareExpectedToGeneratedTraslationJson(
+				GetFilePath(EXPECTED_FILES_PATH, "en.all.json"),
+				GetFilePath(INPUT_FILES_PATH, "en.all.json"),
+			)
+			CompareExpectedToGeneratedTraslationJson(
+				GetFilePath(EXPECTED_FILES_PATH+"/sub", "en.all.json"),
+				GetFilePath(INPUT_FILES_PATH+"/sub", "en.all.json"),
+			)
+		})
+	})
+
+})
