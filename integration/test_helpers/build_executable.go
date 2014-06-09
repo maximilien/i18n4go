5bb75ca4ee9776ff7e6e70d9d190dc96ac13ba46
Anand Gaitonde and Dan Lavine <danlavine@pivotallabs.com>
2014-06-09 13:53:10 -0700

    Change package name to match main executable.

--- integration/test_helpers/build_executable.go
+++ integration/test_helpers/build_executable.go
@@ -10,6 +10,6 @@ var gi18nExec string
 
 func BuildExecutable() {
 	var err error
-	gi18nExec, err = gexec.Build("./../../main")
+	gi18nExec, err = gexec.Build("./../../gi18n")
 	Ω(err).ShouldNot(HaveOccurred())
 }
