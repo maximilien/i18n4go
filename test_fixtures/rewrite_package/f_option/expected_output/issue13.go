a9ba8a6bb38cb293a9522fe90515c73ec9a709f9
dr.max <maximilien@acm.org>
2014-06-20 18:54:48 -0700

    this test case breaks it

--- test_fixtures/rewrite_package/f_option/expected_output/issue13.go
+++ test_fixtures/rewrite_package/f_option/expected_output/issue13.go
@@ -10,5 +10,8 @@ func Issue13() string {
 	someString := T("hello")
 	fmt.Println(someString, T("world"))
 	fmt.Println(someString, T("hello"), T("world"))
+
+	fmt.Println(someString, T("Hello world {{.Arg0}}", map[string]interface{}{"Arg0": T("Hello world {{.Arg0}}")}))
+
 	return fmt.Sprint(10, T("world"))
 }
--- test_fixtures/rewrite_package/f_option/input_files/issue13.go
+++ test_fixtures/rewrite_package/f_option/input_files/issue13.go
@@ -10,5 +10,8 @@ func Issue13() string {
 	someString := "hello"
 	fmt.Println(someString, "world")
 	fmt.Println(someString, "hello", "world")
+
+	fmt.Println(someString, "Hello world %s", someString)
+
 	return fmt.Sprint(10, "world")
 }
