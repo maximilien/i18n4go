53f10c9746b8346a783dd9d44dbb479ced85e3f6
Anand Gaitonde and Devin Fallak <dfallak@pivotallabs.com>
2014-07-10 15:28:17 -0700

    Tests that fail if strings for map keys are added as translated strings.

--- integration/extract_strings/f_option_test.go
+++ integration/extract_strings/f_option_test.go
@@ -84,6 +84,26 @@ var _ = Describe("extract-strings -f fileName", func() {
 		})
 	})
 
+	Context("GitHub issue #16: Extract Strings should ignore string keys in maps", func() {
+		BeforeEach(func() {
+			session := Runi18n("-c", "extract-strings", "-v", "-f", filepath.Join(inputFilesPath, "issue16.go"))
+			Ω(session.ExitCode()).Should(Equal(0))
+		})
+
+		AfterEach(func() {
+			RemoveAllFiles(
+				GetFilePath(inputFilesPath, "issue16.go.en.json"),
+			)
+		})
+
+		It("issue16.go.en.json", func() {
+			CompareExpectedToGeneratedTraslationJson(
+				GetFilePath(expectedFilesPath, "issue16.go.en.json"),
+				GetFilePath(inputFilesPath, "issue16.go.en.json"),
+			)
+		})
+	})
+
 	Context("when the file specified has no strings at all", func() {
 		var (
 			OUTPUT_PATH string
---
+++ test_fixtures/extract_strings/f_option/input_files/issue16.go
@@ -0,0 +1,16 @@
+package app
+
+import "fmt"
+
+func main() {
+	translatedString := "translated"
+
+	T("This is a string which has been {{.Translated}}.", map[string]interface{}{"Translated": translatedString})
+
+	inputs := map[string]string{
+		"test1": "foo",
+	}
+
+	fmt.Println(inputs["test1"])
+
+}
