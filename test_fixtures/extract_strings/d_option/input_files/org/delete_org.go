23535e45ff55b6f4c6ba2cc790234a72adc06f9c
dr.max <maximilien@acm.org>
2014-07-16 16:45:49 -0700

    changing test fixture to remove CF CLI dependencies

--- test_fixtures/extract_strings/d_option/input_files/org/delete_org.go
+++ test_fixtures/extract_strings/d_option/input_files/org/delete_org.go
@@ -1,82 +1,45 @@
 package organization
 
-import ()
+import (
+	"errors"
+	"fmt"
+)
 
 type DeleteOrg struct {
-	ui      terminal.UI
-	config  configuration.ReadWriter
-	orgRepo api.OrganizationRepository
-	orgReq  requirements.OrganizationRequirement
 }
 
-func NewDeleteOrg(ui terminal.UI, config configuration.ReadWriter, orgRepo api.OrganizationRepository) (cmd *DeleteOrg) {
-	cmd = new(DeleteOrg)
-	cmd.ui = ui
-	cmd.config = config
-	cmd.orgRepo = orgRepo
-	return
+type CommandMetadata struct {
+	Name        string
+	ShortName   string
+	Description string
+	Usage       string
 }
 
-func (command *DeleteOrg) Metadata() command_metadata.CommandMetadata {
-	return command_metadata.CommandMetadata{
+type BoolFlag struct {
+	Name  string
+	Usage string
+}
+
+func (command *DeleteOrg) Metadata() CommandMetadata {
+	return CommandMetadata{
 		Name:        "delete-org",
 		Description: "Delete an org",
 		Usage:       "CF_NAME delete-org ORG [-f]",
-		Flags: []cli.Flag{
-			cli.BoolFlag{Name: "f", Usage: "Force deletion without confirmation"},
-		},
+		Flags:       fmt.Printf(BoolFlag{Name: "f", Usage: "Force deletion without confirmation"}),
 	}
 }
 
-func (cmd *DeleteOrg) GetRequirements(requirementsFactory requirements.Factory, c *cli.Context) (reqs []requirements.Requirement, err error) {
-	if len(c.Args()) != 1 {
-		err = errors.New("Incorrect Usage")
-		cmd.ui.FailWithUsage(c)
-		return
-	}
-
-	reqs = []requirements.Requirement{requirementsFactory.NewLoginRequirement()}
-	return
+func (cmd *DeleteOrg) GetRequirements(err error) {
+	err = errors.New("Incorrect Usage")
 }
 
-func (cmd *DeleteOrg) Run(c *cli.Context) {
-	orgName := c.Args()[0]
+func (cmd *DeleteOrg) Run() {
+	var orgName, username string
 
-	if !c.Bool("f") {
-		if !cmd.ui.ConfirmDeleteWithAssociations("org", orgName) {
-			return
-		}
-	}
+	fmt.Println("f")
+	fmt.Println("org", orgName)
 
-	cmd.ui.Say("Deleting org %s as %s...",
-		terminal.EntityNameColor(orgName),
-		terminal.EntityNameColor(cmd.config.Username()),
-	)
-
-	org, apiErr := cmd.orgRepo.FindByName(orgName)
-
-	switch apiErr.(type) {
-	case nil:
-	case *errors.ModelNotFoundError:
-		cmd.ui.Ok()
-		cmd.ui.Warn("Org %s does not exist.", orgName)
-		return
-	default:
-		cmd.ui.Failed(apiErr.Error())
-		return
-	}
-
-	apiErr = cmd.orgRepo.Delete(org.Guid)
-	if apiErr != nil {
-		cmd.ui.Failed(apiErr.Error())
-		return
-	}
-
-	if org.Guid == cmd.config.OrganizationFields().Guid {
-		cmd.config.SetOrganizationFields(models.OrganizationFields{})
-		cmd.config.SetSpaceFields(models.SpaceFields{})
-	}
-
-	cmd.ui.Ok()
+	fmt.Printf("Deleting org %s as %s...", orgName, username)
+	fmt.Printf("Org %s does not exist.", orgName)
 	return
 }
