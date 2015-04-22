b3a1caa7358087aa32cc228e2956e1287f17b3ed
dr.max <maximilien@acm.org>
2015-04-22 14:31:47 -0700

    added first demo for GoSF presentation

---
+++ examples/demo1/demo1.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+const VERSION = "v0.0.1"
+
+func main() {
+	fmt.Println("Hello from Goffer land and i18n4go")
+	fmt.Println("")
+	fmt.Printf("Version %s\n", VERSION)
+}
