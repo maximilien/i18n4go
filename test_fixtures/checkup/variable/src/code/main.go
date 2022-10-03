a91ab08e19cb94a06d2b44fda5684ef9b07ab95b
Aerex <noamfo@aerex.me>
2022-10-03 11:22:04 -0500

    test(checkup): Updated test to show using multiple reassigned variables

--- test_fixtures/checkup/variable/src/code/main.go
+++ test_fixtures/checkup/variable/src/code/main.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	locale := "Translated hello world!"
 	fmt.Println(T(locale))
+	locale = "For you and for me"
+	fmt.Println(T(locale))
 	locale = "I like bananas"
 	fmt.Println(T(locale))
 }
